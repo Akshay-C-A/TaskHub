460d039afda441c71ab59f6994a9dec92c728e90
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-05-13 19:25:27 +0530

    Updated leader

--- taskhubapp/lib/teamLeader/leaderDashboard.dart
+++ taskhubapp/lib/teamLeader/leaderDashboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taskhubapp/teamLeader/addTask.dart';
 import 'package:taskhubapp/teamLeader/sample.dart';
+import 'package:taskhubapp/teamLeader/task_posted.dart';
 
 class LeaderDashboard extends StatefulWidget {
   const LeaderDashboard({super.key});
@@ -15,8 +16,7 @@ class LleaderDashboardState extends State<LeaderDashboard> {
   @override
   Widget build(BuildContext context) {
     List<Widget> widgetOptions = <Widget>[
-      const Sample(),
-      const Sample(),
+      const TaskPosted(),
       const AddTaskPage(
         projectName: '123B65',
         leaderId: 'leader@company.com',
@@ -26,6 +26,15 @@ class LleaderDashboardState extends State<LeaderDashboard> {
     ];
 
     return Scaffold(
+      floatingActionButton: Badge(
+        isLabelVisible: true,
+        backgroundColor: Colors.red,
+        label: Text('1'),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.chat),
+        ),
+      ),
       body: IndexedStack(
         index: _selectedIndex,
         children: widgetOptions,
@@ -46,16 +55,12 @@ class LleaderDashboardState extends State<LeaderDashboard> {
             label: 'Home',
           ),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.people),
-            label: 'Students',
-          ),
-          const BottomNavigationBarItem(
             icon: Icon(Icons.add),
             label: 'Post',
           ),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: 'Notification',
+            icon: Icon(Icons.person_add_alt),
+            label: 'Add Member',
           ),
         ],
       ),

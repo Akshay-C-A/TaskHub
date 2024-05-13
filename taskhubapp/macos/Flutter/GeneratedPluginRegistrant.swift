e88e5d6aa9ec7a9c0595e71d6fabb406e5386d54
AmiyaHidayathulla <amiyahiday208@gmail.com>
2024-05-14 00:02:56 +0530

    updated

--- taskhubapp/macos/Flutter/GeneratedPluginRegistrant.swift
+++ taskhubapp/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,8 +9,8 @@ import cloud_firestore
 import file_selector_macos
 import firebase_auth
 import firebase_core
-import flutter_local_notifications
 import firebase_storage
+import flutter_local_notifications
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -19,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }

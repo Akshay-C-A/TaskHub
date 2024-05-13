ebb5a9e164b4ce780988c5979b228368fec344a9
AmiyaHidayathulla <amiyahiday208@gmail.com>
2024-05-13 23:52:05 +0530

    profile form updated

--- taskhubapp/lib/main.dart
+++ taskhubapp/lib/main.dart
@@ -4,9 +4,16 @@ import 'package:taskhubapp/auth/login_page.dart';
 // import 'package:taskhubapp/auth/login_page.dart';
 import 'package:taskhubapp/TeamMember/memberDashboard.dart';
 import 'firebase_options.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  await Permission.photos.request();
+  await Permission.storage.request();
+
+  
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
